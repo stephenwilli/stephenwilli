4db8b5571df2bc8281d9397e4d29ce28a4678808
Stephen Williams <stephen@wearetmbr.com>
2021-05-13 18:10:39 -0600

    next/prev

--- wp-content/themes/stephenwilli/partials/02_molecule/related-posts.php
+++ wp-content/themes/stephenwilli/partials/02_molecule/related-posts.php
@@ -1,12 +1,16 @@
 <section class="related-posts">
   <div class="posts-wrap">
   <?php
-    $prevPost = get_previous_post();?>
+    $prevPost = get_previous_post();
+    if($prevPost) { 
+      $prevUrl = get_permalink($prevPost->ID);
+    }
+    ?>
     <div class="post-link -prev">
       <?php if($prevPost){ 
         $prevThumbnail = get_the_post_thumbnail_url($prevPost->ID, 'large' );
         ?>
-          <a href="<?php echo $prevPost->guid ?>">
+          <a href="<?= $prevUrl; ?>">
             <h4>Previous Post</h4>
             <h2><?php echo $prevPost->post_title ?></h2>
           </a>
@@ -14,11 +18,15 @@
     </div>
     
     <?php
-    $nextPost = get_next_post();?>
+    $nextPost = get_next_post();
+    if($nextPost) { 
+      $nextUrl = get_permalink($nextPost->ID);
+    }
+    ?>
       <div class="post-link -next">
         <?php if($nextPost) { 
           $nextThumbnail = get_the_post_thumbnail_url($nextPost->ID, 'large' );?>
-            <a href="<?php echo $nextPost->guid ?>">
+            <a href="<?= $nextUrl; ?>">
               <h4>Next Post</h4>
               <h2><?php echo $nextPost->post_title ?></h2>
             </a>
--- wp-content/themes/stephenwilli/partials/02_molecule/related-series.php
+++ wp-content/themes/stephenwilli/partials/02_molecule/related-series.php
@@ -1,13 +1,14 @@
 <section class="related-posts">
   <div class="posts-wrap">
   <?php
-    $prevSeries = get_field('prev_series');
+    $prevSeries = get_field('previous_series');
+    $prevUrl = get_permalink($prevSeries->ID);
     ?>
     <div class="post-link -prev">
       <?php if($prevSeries){ 
         $prevThumbnail = get_the_post_thumbnail_url($prevSeries->ID, 'large' );
         ?>
-          <a href="<?php echo $prevSeries->guid ?>">
+          <a href="<?= $prevUrl;?>">
             <h4>Previous Series</h4>
             <h2><?php echo $prevSeries->post_title ?></h2>
           </a>
@@ -15,11 +16,13 @@
     </div>
     
     <?php
-    $nextSeries = get_field('nex_series');?>
+    $nextSeries = get_field('next_series');
+    $nextUrl = get_permalink($nextSeries->ID);
+    ?>
       <div class="post-link -next">
         <?php if($nextSeries) { 
           $nextThumbnail = get_the_post_thumbnail_url($nextSeries->ID, 'large' );?>
-            <a href="<?php echo $nextSeries->guid ?>">
+            <a href="<?= $nextUrl;?>">
               <h4>Next Series</h4>
               <h2><?php echo $nextSeries->post_title ?></h2>
             </a>
