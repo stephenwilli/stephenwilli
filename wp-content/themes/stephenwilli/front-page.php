5512b6430419a59a655c2197c48758b1456e6111
Stephen Williams <stephen@wearetmbr.com>
2021-06-03 09:17:02 -0600

    home switch

--- wp-content/themes/stephenwilli/front-page.php
+++ wp-content/themes/stephenwilli/front-page.php
@@ -4,15 +4,27 @@
   
   <div id="site-loader">
     <div class="loader-wrap">
+      <a href></a>
       <?php echo file_get_contents(get_template_directory_uri() . '/assets/images/sw-03.svg'); ?>
       <h1 class="loader-heading" id="loader-heading" data-split-text>Stephen Williams Photography</h1>
     </div>
   </div>
   
-  <?php get_template_part( 'partials/03_organism/home-hero' ); ?>
+  <div class="photo-home">
+    <?php get_template_part( 'partials/03_organism/home-hero' ); ?>
+    
+    <div class="inner-wrap">
+      <div class="inner-border">
+      </div>
+    </div>
+  </div>
   
-  <div class="inner-wrap">
-    <div class="inner-border">
+  <div class="dev-home">
+    <?php get_template_part( 'partials/03_organism/dev-hero' ); ?>
+    
+    <div class="inner-wrap">
+      <div class="inner-border">
+      </div>
     </div>
   </div>
   
