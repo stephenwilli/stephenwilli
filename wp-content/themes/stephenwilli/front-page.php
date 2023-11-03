9b1f1b6ae52c185adfb3c00b60fe6844115f5479
Stephen Williams <stephen@wearetmbr.com>
2023-11-02 18:06:26 -0600

    loader

--- wp-content/themes/stephenwilli/front-page.php
+++ wp-content/themes/stephenwilli/front-page.php
@@ -1,11 +1,11 @@
 <?php get_header() ?>
 
-<!-- <div id="site-loader">
+<div id="site-loader">
     <div class="loader-wrap">
-        <?php // echo file_get_contents(get_template_directory_uri() . '/assets/images/sw-03.svg'); ?>
+        <?php echo file_get_contents(get_template_directory_uri() . '/assets/images/sw-03.svg'); ?>
         <h1 class="loader-heading" id="loader-heading" data-split-text>Stephen Williams Photography</h1>
     </div>
-</div> -->
+</div>
   
 <?php render('home-hero'); ?>
   
