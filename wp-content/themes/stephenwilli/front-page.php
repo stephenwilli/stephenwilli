6716e867642de148160344f63bfc273215e9a842
Stephen Williams <stephen@wearetmbr.com>
2024-07-30 13:03:06 -0600

    intro

--- wp-content/themes/stephenwilli/front-page.php
+++ wp-content/themes/stephenwilli/front-page.php
@@ -3,7 +3,8 @@
 <div id="site-loader">
     <div class="loader-wrap">
         <?php echo file_get_contents(get_template_directory_uri() . '/assets/images/sw-03.svg'); ?>
-        <h3 class="loader-title">Stephen Williams <br> Photographer / Creative Developer</h3>
+        <h3 class="loader-title">Stephen Williams</h3>
+        <h4 class="loader-title">Photographer / Creative Developer</h4>
     </div>
 </div>
   
