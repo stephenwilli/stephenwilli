7139cec130b260736c7b4fb45fdd4cedc13cf724
Stephen Williams <stephen@wearetmbr.com>
2024-08-14 10:03:56 -0600

    screen size

--- wp-content/themes/stephenwilli/partials/related-projects.php
+++ wp-content/themes/stephenwilli/partials/related-projects.php
@@ -2,13 +2,10 @@
   <div class="posts-wrap">
   <?php
     $prevPost = get_previous_post();
-    if($prevPost) { 
-      $prevUrl = get_permalink($prevPost->ID);
-    }
     ?>
     <div class="post-link -prev">
       <?php if($prevPost){ 
-        $prevThumbnail = get_the_post_thumbnail_url($prevPost->ID, 'large' );
+        $prevUrl = get_permalink($prevPost->ID);
         ?>
           <a href="<?= $prevUrl; ?>">
             <h4>Previous Project</h4>
@@ -20,13 +17,11 @@
     
     <?php
     $nextPost = get_next_post();
-    if($nextPost) { 
-      $nextUrl = get_permalink($nextPost->ID);
-    }
     ?>
       <div class="post-link -next">
         <?php if($nextPost) { 
-          $nextThumbnail = get_the_post_thumbnail_url($nextPost->ID, 'large' );?>
+          $nextUrl = get_permalink($nextPost->ID);
+          ?>
             <a href="<?= $nextUrl; ?>">
               <h4>Next Project</h4>
               <h2><?php echo $nextPost->post_title ?></h2>
