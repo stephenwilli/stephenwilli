71ad2664ed84156fdd9581aafdb883cdc45ff10d
Stephen Williams <stephen@wearetmbr.com>
2024-11-15 14:56:07 -0700

    projects

--- wp-content/themes/stephenwilli/single-projects.php
+++ wp-content/themes/stephenwilli/single-projects.php
@@ -10,7 +10,9 @@
 
 <section class="project-hero" style="background-image: url('<?= $postImage;?>');">
   <div class="overlay"></div>
-  <h1 class="h2"><?php the_title();?></h1>
+  <div class="project-title">
+    <h1><?php the_title();?></h1>
+  </div>
 </section>
 
   <section class="content-wrap container pad-b" data-animate="fade-right">
